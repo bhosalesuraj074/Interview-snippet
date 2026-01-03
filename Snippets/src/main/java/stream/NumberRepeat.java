62d37ad67d4db147bc6a829f5e5cb6ddde526c05
Suraj Bhosale <125651189+bhosalesuraj074@users.noreply.github.com>
2026-01-03 08:44:51 +0530

    Refactor NumberRepeat to use intermediate variables
    Introduced a named list variable and separated the collection of grouped counts into a distinct variable for improved readability and maintainability.

--- Snippets/src/main/java/stream/NumberRepeat.java
+++ Snippets/src/main/java/stream/NumberRepeat.java
@@ -6,9 +6,13 @@ import java.util.stream.Collectors;
 
 public class NumberRepeat {
     public static void main(String[] args) {
-        List.of(12,35,37,45,23,97,56,12)
+        List<Integer> list =  List.of(12,35,37,45,23,97,56,12);
+
+        Map<Integer, Long> collect = list
                 .stream()
-                .collect(Collectors.groupingBy(num-> num, Collectors.counting()))
+                .collect(Collectors.groupingBy(num -> num, Collectors.counting()));
+
+        collect
                 .entrySet()
                 .stream()
                 .filter(integerLongEntry -> integerLongEntry.getValue()> 1)
